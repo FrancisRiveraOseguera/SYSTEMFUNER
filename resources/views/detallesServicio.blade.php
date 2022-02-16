0b1da0798ae7d7b425f3788832e0857d17ab33cd
Eleana Cano <eleanacano1@gmail.com>
2022-02-15 22:00:53 -0600

    Update detallesServicio.blade.php

--- resources/views/detallesServicio.blade.php
+++ resources/views/detallesServicio.blade.php
@@ -3,109 +3,97 @@
 
 @section('content')
 
-<style>
-    #IcNewServ{
-        font-size:30px;
-        width: 1em;
-        height: 1em;
-    }
-    .servfun {
-  border-top: 1px solid #E6E6E6 ;
-  border-left: 1px solid #E6E6E6 ;
-  border-right: 1px solid #E6E6E6;
-  border-bottom: 1px solid #E6E6E6 ;
-  padding: 20px;
-  background-color: #E0F8F7;
-  position:relative;
-   }
-
-   .servfu{
-       font-style: bold;
-       font-family: 'Times New Roman', Times, serif;
-   }
-</style>
-
-<div class="servfun">
-    <h1 class="servfu text-center">Detalles del Servicio</h1>
-</div>
-<!--Contenedor para los datos-->
-<div class="servfun w-100">
-
-    <form class="form-outline w-100">
-        <div class="form-group">
-            <div class="form-inline">
-                <label class="form-label col-lg-3 offset-md-1" for="tipo">
-                    <i id="IcNewServ" class="bi bi-archive"></i> Tipo de servicio
-                </label>
-                <input type="text" id="tipo" class="form-control col-sm-6" name="tipo"
-                       value="{{$Servicio->tipo}}" readonly/>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="form-inline">
-                <label class=" form-label col-lg-3 offset-md-1" for="precio">
-                    <i id="IcNewServ" class="bi bi-cash-coin"></i>Precio del servicio
-                </label>
-                <input type="text" id="precio" class="form-control col-sm-6"  name="precio"
-                       value="{{$Servicio->precio}}" readonly/>
-            </div>
-        </div>
-
-
-        <div class="form-group">
-            <div class="form-inline">
-                <label class="form-label col-lg-3 offset-md-1" for="cuota">
-                    <i id="IcNewServ" class="bi bi-currency-dollar"></i>Cuota
-                </label>
-                <input type="text" id="cuota" class="form-control col-sm-6"  name="cuota"
-                       value="{{$Servicio->cuota}}" readonly/>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="form-inline">
-                <label class="form-label col-lg-3 offset-md-1" for="prima">
-                    <i id="IcNewServ" class="bi bi-coin"></i>Prima del servicio
-                </label>
-                <input type="text" id="prima" class="form-control col-sm-6"  name="prima"
-                       value="{{$Servicio->prima}}" readonly/>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="form-inline">
-                <label class="form-label col-lg-3 offset-md-1 text" for="categoria">
-                    <i  id="IcNewServ" class="bi bi-list-stars"></i>Categoría
-                </label>
-                <input type="text" id="categoria" class="form-control col-sm-6"  name="categoria"
-                       value="{{$Servicio->categoria}}" readonly/>
-            </div>
-        </div>
-
-
-        <div class="form-group">
-            <div class="form-inline">
-                <label class="form-label col-lg-3 offset-md-1" for="detalles">
-                    <i id="IcNewServ" class="bi bi-pencil-square"></i>Detalles
-                </label>
-                <div class="col-sm-6">
-                        <textarea  name="detalles"  id="detalles"  cols="52" rows="3"
-                                   readonly>{{$Servicio->detalles}}
-                    </textarea>
+    <!--Contenedor para ver la información del tipo de servicio-->
+    <div class="emple">
+        <h3> Detalles del tipo de Servicio</h3>
+        <hr>
+        <div class="emple">
+            <form class="form-outline">
+                @csrf
+                <div class="form-group row">
+                    <label for="tipo" class="col-lg-3 control-label offset-md-1 requerido hijo">
+                        <i id="IcNewServ" class="bi bi-archive"></i>Tipo de servicio:
+                    </label>
+                    <div class="col-sm-8">
+                        {{$Servicio->tipo}}
+                    </div>
                 </div>
-            </div>
+
+                <div class="form-group row">
+                    <label for="precio" class="col-lg-3 control-label offset-md-1 requerido hijo">
+                        <i id="IcNewServ" class="bi bi-cash-coin"></i>Precio del servicio:
+                    </label>
+                    <div class="col-sm-8">
+                        {{$Servicio->precio}}
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="cuota" class="col-lg-3 control-label offset-md-1 requerido hijo">
+                        <i id="IcNewServ" class="bi bi-currency-dollar"></i>Cuota:
+                    </label>
+                    <div class="col-sm-8">
+                        {{$Servicio->cuota}}
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="prima" class="col-lg-3 control-label offset-md-1 requerido hijo">
+                        <i id="IcNewServ" class="bi bi-coin"></i>Prima del servicio:
+                    </label>
+                    <div class="col-sm-8">
+                        {{$Servicio->prima}}
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="categoria" class="col-lg-3 control-label offset-md-1 requerido hijo">
+                        <i  id="IcNewServ" class="bi bi-list-stars"></i>Categoría:
+                    </label>
+                    <div class="col-sm-8">
+                        {{$Servicio->categoria}}
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="detalles" class="col-lg-3 control-label offset-md-1 requerido hijo">
+                        <i id="IcNewServ" class="bi bi-pencil-square"></i>Detalles:
+                    </label>
+                    <div class="col-sm-8">
+                        {{$Servicio->detalles}}
+                    </div>
+                </div>
+                <br>
+
+                <!--Contenedor para los botones-->
+                <div class="col-md-12 ">
+                    <a class="btn btn-primary " href="#" > <i class="fas fa-user"></i>Ver Clientes</a>
+                    <a class="btn btn-warning " href="{{route('Servicio.lista')}}" > <i class="bi bi-box-arrow-left"></i>Regresar</a>
+                </div>
+            </form>
         </div>
+    </div>
 
-        
-        <br>
 
-        <!--Contenedor para los botones-->
-        <div class="col-md-12 text-center ">
-            <a class="btn btn-primary " href="#" > <i class="bi bi-x-octagon"></i>Ver Clientes</a>
-            <a class="btn btn-warning " href="{{route('Servicio.lista')}}" > <i class="bi bi-x-octagon"></i>Regresar</a>
-        </div><br>
-    </form>
-</div>
+    <style>
+        .emple {
+            border-top: 1px solid #E6E6E6 ;
+            border-left: 1px solid #E6E6E6 ;
+            border-right: 1px solid #E6E6E6;
+            border-bottom: 1px solid #E6E6E6 ;
+            padding: 20px;
+            background-color: #E0F8F7;
+            position:relative;
+        }
+
+        .emple{
+            font-style: bold;
+            font-family: 'Times New Roman', Times, serif;
+        }
+
+        .hijo{
+            font-weight: bold;
+        }
+    </style>
 
 @endsection
