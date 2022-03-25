925c341fb77c3ba9ed98d45924bc5751d909e0d8
Eleana Cano <eleanacano1@gmail.com>
2022-03-25 16:32:11 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -165,6 +165,9 @@ Route::post('/NuevaVentaAlCredito{newcl?}', 'App\Http\Controllers\creditoventaCo
 Route::get('/listadoVentasCredito', 'App\Http\Controllers\creditoventaController@index')
     ->name('ventasCredito.index');
 
+Route::get('/ventaCredito/detalles/{id}', 'App\Http\Controllers\creditoventaController@show')
+    ->name('ventaCredito.ver')
+    ->where('id', '[0-9]+');
 
 //RUTAS DE NUEVO PAGO
 Route::get('/nuevoPago/{id}',[PagosController::class, 'create'])->name('nuevoPagos.nuevo');
