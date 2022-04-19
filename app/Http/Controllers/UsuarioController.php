bdf64d2fed436a810cc59047280aeb905ef9ea78
CindySalgado <cindysalgado@unah.hn>
2022-04-19 10:28:02 -0600

    Commit actualizado del listado de usuarios

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -160,4 +160,12 @@ class UsuarioController extends Controller
         }
 
     }
+
+    //Eliminar usuario
+    public function destroy($id)
+    {
+        Usuario::destroy($id);
+
+        return redirect()->route('listado.usuario')->with('mensaje', 'El usuario fue eliminado exitosamente');
+    }
 }
