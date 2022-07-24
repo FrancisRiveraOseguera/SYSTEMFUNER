c5d9345b056b412ac7e7346ffb6301c54dcd9a4c
carlosrl19 <lcarloseduardo19@gmail.com>
2022-07-24 12:53:22 -0600

    Agregué guía para horario militar al crear turno

--- resources/views/turnos/crearTurno.blade.php
+++ resources/views/turnos/crearTurno.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="emple">
-    <h3> Nuevo turno</h3>
+    <h3> Nuevo turno</h3> 
     <hr>
 @if ($errors->any())
 <div class="alert alert-danger alert-dismissible" data-auto-dismiss="3000">
@@ -20,7 +20,32 @@
 <div class="emple">
     <form method="post" action="" autocomplete="off">
         @csrf
-		<p style="background: rgba(227,223,168,0.4); padding: 5px; font-size: 13px; font-weight: bold; color: green; border-radius: 10px">Nota: Para evitar confusiones al ingresar los horarios, se trabaja con el sistema militar de 24 horas (Ejemplo: 1:00pm es igual a 13:00).</p>
+		<p style="background: rgba(227,223,168,0.4); padding: 5px; font-size: 13px; font-weight: bold; color: green; border-radius: 10px">
+			Nota: Para evitar confusiones al ingresar los horarios, se trabaja con el sistema militar de 24 horas (Ejemplo: 1:00pm es igual a 13:00).
+		<a class="btn" style="color: white; background: rgba(0, 180, 226, 0.8); margin-left: 3%" data-toggle="modal" data-target="#modalPushG">
+			<i class="bi bi-info-circle"> Ayuda</i>
+		</a>
+		</p>
+
+            <!--Modal: modalPush-->
+            <div class="modal fade" tabindex="1" id="modalPushG" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+
+            <div class="modal-dialog modal-notify modal-info" role="document">
+                    <!--Content-->
+                    <div class="modal-content text-center">
+                    <!--Header-->
+                    <div class="modal-header d-flex justify-content-center">
+                        <p class="heading">Tabla guía para asignar horas</p>
+                    </div>
+
+                    <!--Body-->
+                    <div class="modal-body">
+                        <img src="../../assets/horario-militar.jpg" width="450" height="500">
+                    </div>
+                </div>
+            </div>
+    </div>
+
 		<div class="form-group row">
 				<label for="name" class="col-lg-3 control-label offset-md-1 requerido">
 				<i  id="IcNewEmp" class="bi bi-person-badge"></i>Nombre del turno</label>
@@ -39,7 +64,7 @@
 			<div class="col-sm-8">
 				<input type="time" style="margin-left: -13%; width: 100%"
 					oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"
-					name="horario_entrada" id="horario_entrada"  maxlength = "8"
+					name="horario_entrada" id="horario_entrada"  maxlength="5"
 					placeholder="Horario de entrada que tendrá el empleado con este horario" class="form-control"
 					value="{{old('horario_entrada', $turnos->horario_entrada ?? '')}}"
 				/>
@@ -52,7 +77,7 @@
 			<div class="col-sm-8">
 				<input type="time" style="width: 100%"
 					oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"
-					name="horario_salida" id="horario_salida"  maxlength = "8"
+					name="horario_salida" id="horario_salida"  maxlength="5"
 					placeholder="Horario de salida que tendrá el empleado con este horario" class="form-control"
 					value="{{old('horario_salida', $turnos->horario_salida ?? '')}}"
 					/>
